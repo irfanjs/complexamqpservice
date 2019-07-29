48e45fd07b58ef605743761041d389da8c0db605
Sayed <Irfan.Sayed@Honeywell.com>
2019-07-29 16:48:59 +0530

    first commit

---
+++ src/main/java/com/infy/ci/complexamqpservice/ComplexamqpserviceApplication.java
@@ -0,0 +1,13 @@
+package com.infy.ci.complexamqpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexamqpserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplexamqpserviceApplication.class, args);
+	}
+
+}
